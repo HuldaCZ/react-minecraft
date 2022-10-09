1c43a969cb440dad2e34e01d326832e0241c6bb4
Petr Hulínský <petr.hulinsky@koala42.com>
2022-10-09 19:24:35 +0200

    Ground

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { Physics } from '@react-three/cannon';
+import { Sky } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
+import { Ground} from './components/Ground.js';
 
 function App() {
   return (
     <>
-      <div>Outside Canvas</div>
+      {/* <div>Outside Canvas</div> */}
       <Canvas>
+        <Sky sunPosition={[200, 200, 50]} />
+        <ambientLight intensity={0.5} />
+        <Physics>
+          <Ground />
+        </Physics>
       </Canvas>
     </>
   );
---
+++ src/images/textures.js
@@ -0,0 +1,12 @@
+import { TextureLoader } from "three";
+
+import { dirtImg, grassImg, glassImg, woodImg, logImg } from "./images.js";
+
+const dirtTexture = new TextureLoader().load(dirtImg);
+const grassTexture = new TextureLoader().load(grassImg);
+const glassTexture = new TextureLoader().load(glassImg);
+const woodTexture = new TextureLoader().load(woodImg);
+const logTexture = new TextureLoader().load(logImg);
+const groundTexture = new TextureLoader().load(grassImg);
+
+export { dirtTexture, grassTexture, glassTexture, woodTexture, logTexture, groundTexture };
